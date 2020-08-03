4fb570d63ebffbb010e0b1fd98898e500c21b379
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-08-03 17:54:25 +0300

    добавляет задачу на пустой обьект

---
+++ task11.js
@@ -0,0 +1,23 @@
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+// Должно работать так:
+
+// let schedule = {};
+
+// alert(isEmpty(schedule)); // true
+
+// schedule["8:30"] = "get up";
+
+// alert(isEmpty(schedule)); // false
+
+// Решение
+
+// Просто в цикле перебираем свойства объекта и возвращаем false, как только встречаем свойство.
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // если тело цикла начнет выполняться - значит в объекте есть свойства
+    return false;
+  }
+  return true;
+}
